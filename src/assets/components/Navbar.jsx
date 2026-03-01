9af1c706d6a651cd6e8c94fc4619b6dfbe833b28
azazel <azazeljin69@gmail.com>
2026-03-01 14:04:38 +0800

    responsiveness

--- src/assets/components/Navbar.jsx
+++ src/assets/components/Navbar.jsx
@@ -1,33 +1,47 @@
 import { useState } from "react";
 import "./Navbar.css";
 import logo from "../images/logo.png";
+
 function Navbar() {
-    const [isOpen, setIsOpen] = useState(false);   
-    const toggleMenu = () => {
-        setIsOpen(!isOpen);
+  const [isOpen, setIsOpen] = useState(false);
 
+  // toggle menu function
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
 
-    };
-    return (
-        <nav className="navbar navbar-expand-sm fixed-top pd">
-            <div className="navbar-container d-flex align-items-center justify-content-between" >
-                <a href="/" className="navbar-logo d-flex align-items-center">
-                <img src={logo} alt="this is the company logo"/>
-                </a>   
-                <div className={`navbar-menu d-flex gap-3 ${isOpen ? "open" : ""}`}>
-                    <a href="/home" className="navbar-item">ABOUT</a>
-                    <a href="/about" className="navbar-item">SKILLS</a>
-                    <a href="/services" className="navbar-item">PROJECTS</a>
-                    <a href="/contact" className="navbar-item">CONTACT</a>
-                </div>
-                <div className="navbar-toggle" onClick={toggleMenu}>
-                    <span className="bar"></span>
-                    <span className="bar"></span>
-                    <span className="bar"></span>
-                </div>
-            </div>
-        </nav>
-    );
+  return (
+    <nav className="navbar navbar-expand-sm fixed-top pd">
+      <div className="navbar-container d-flex align-items-center justify-content-between">
+        <a href="/" className="navbar-logo d-flex align-items-center">
+          <img src={logo} alt="Company logo" />
+        </a>
+
+        {/* Navbar menu */}
+        <div className={`navbar-menu d-flex gap-3 ${isOpen ? "active" : ""}`}>
+          <a href="/home" className="navbar-item">
+            ABOUT
+          </a>
+          <a href="/about" className="navbar-item">
+            SKILLS
+          </a>
+          <a href="/services" className="navbar-item">
+            PROJECTS
+          </a>
+          <a href="/contact" className="navbar-item">
+            CONTACT
+          </a>
+        </div>
+
+        {/* Hamburger */}
+        <div className="navbar-toggle" onClick={toggleMenu}>
+          <span className="bar"></span>
+          <span className="bar"></span>
+          <span className="bar"></span>
+        </div>
+      </div>
+    </nav>
+  );
 }
 
-export default Navbar;  
+export default Navbar;
